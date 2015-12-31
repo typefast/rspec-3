38dbe13d2789227057b8c439b3126923a107b2bf
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-31 12:25:54 +0000

    Add zombie new test

---
+++ spec/zombie_spec.rb
@@ -0,0 +1,15 @@
+require 'zombie'
+
+Rspec.describe Zombie do 
+  
+  before :each do 
+    @zombie = Zombie.new
+  end
+
+  describe "#new" do 
+    it "creates a new instance of Zombie" do 
+      expect(@zombie).to be_an_instance_of(Zombie)
+    end
+  end
+
+end
